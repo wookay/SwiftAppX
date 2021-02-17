5985ffb0f3613342e2f771011466c797d5e9ee5b
WooKyoung Noh <wookay.noh@gmail.com>
2021-02-18 00:27:28 +0900

    init

---
+++ AppX/AppX/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  AppX
+//
+//  Created by ㅊㅂㄸ on 2021/02/18.
+//
+
+import UIKit
+import SwiftPkgA
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+
+        SwiftPkgA.hello()
+    }
+
+
+}
+
